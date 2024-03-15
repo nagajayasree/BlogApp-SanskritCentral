bfef53c95841eb58f90f79c341c50ee91bc06367
JayaSree <js@eventdesk.co>
2024-03-14 20:02:59 -0400

    feeds css

--- client/src/components/feeds.js
+++ client/src/components/feeds.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_FEEDS } from '../queries/getFeeds';
 import FeedCard from './FeedCard';
-import { Space } from 'antd';
 
 function Feeds() {
   const { loading, error, data } = useQuery(GET_FEEDS);
@@ -11,14 +10,22 @@ function Feeds() {
   if (error) return `Error! ${error.message}`;
 
   return (
-    <div style={{ alignContent: 'center', margin: 20 }}>
-      <Space direction="vertical" size={16}>
+    <div style={{ margin: 20 }}>
+      <div
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          listStyle: 'none',
+          flexDirection: 'column',
+          gap: 16,
+        }}
+      >
         {data.getFeeds.map((feed) => (
           <return>
             <FeedCard title={feed.title} content={feed.content} />
           </return>
         ))}
-      </Space>
+      </div>
     </div>
   );
 }
